00d7901d43538a8fe4defb2360733e019c54a4fd
rutu14 <51828157+rutu14@users.noreply.github.com>
2021-08-17 17:25:56 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,37 +1,33 @@
 var bill=document.querySelector("#bill-amt")
 var cash=document.querySelector("#cash-given")
-var btn=document.querySelector(#btn)
-
-url_server="https://api.funtranslations.com/translate/minion.json"
-function gettranslation(input){
-    return url_server+"?"+"text="+input
-}
+var btn=document.querySelector("#btn")
+var noofnote=document.querySelectorAll(".notes")
 
 btn.addEventListener("click",function clickEventHandler(){
     var input1=bill.value
+    console.log("bill:"+input1)
     var input2=cash.value
-    if (cash>bill) && (bill.isInteger()){
-        back=cash-bill
-        notes=[2000, 500, 100, 20, 10, 5, 1]
-        while(back<=0){
-            for (i=0;i<notes.length;i++ ){
-                amt=back/notes[i]
-                console.log(amt)
-                back=back%notes[i]
-                console.log(back)
-
-            }
-        }
-    }
+    console.log("cash:"+input2)
+    if (input2>input1 && input1>0){
+        back=input2-input1
+        console.log("baln"+back)}
     else{
         alert("Invalid Amount given. ")
-    }
-    // fetch(gettranslation(input))
-    //     .then(response => response.json())
-    //     .then(json=>{
-    //         outtxt.innerText=json.contents.translated})
-    //     .catch(function errorHandler(error){
-    //         console.log("Server Error.")
-    //         alert("Server error!")
-    //     })
-})
+        }
+    notes=[2000, 500, 100, 20, 10, 5, 1]
+    var bt=[]
+    while(back!=0){
+            for (i=0;i<notes.length;i++ ){
+                console.log("notes"+notes[i])
+                amt=Math.floor(back/notes[i])
+                bt[i]=amt
+                console.log("qt"+amt)
+                rem=back%notes[i]
+                console.log("re"+rem)
+                back=back-(notes[i]*amt)
+                noofnote[i].innerText=bt[i]
+                }
+            }
+      }
+    
+)
